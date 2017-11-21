8fed061e5385c9b981fcd0cd4c2926ea7f9928d7
jnprogrammer <joshuanparker@gmail.com>
2017-11-21 14:13:09 -0800

    Added Classes exercise

---
+++ c#/Intro to c#/classes/classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using classes.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+     
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calc = new Calculator();
+
+            var result = calc.Add(23, 5);
+            Console.WriteLine(result);
+        }
+    }
+}
