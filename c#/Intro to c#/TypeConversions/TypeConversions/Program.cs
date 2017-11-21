738c42a04968b044b26735cbb966cb88b380d849
jnprogrammer <joshuanparker@gmail.com>
2017-11-20 19:51:25 -0800

    Adding conversion project

---
+++ c#/Intro to c#/TypeConversions/TypeConversions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implicit Type Conversion
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+
+            //Explicit Type Conversion
+            int r = 1;
+            byte u = (byte)r;
+            Console.WriteLine(u);
+
+
+            var num = "1234";
+            int l = Convert.ToInt32(num);
+            Console.WriteLine(l);
+
+        }
+    }
+}
