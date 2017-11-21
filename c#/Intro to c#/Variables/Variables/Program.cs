1c57556efd44e9171101df26e98b568d1cc2d39c
jnprogrammer <joshuanparker@gmail.com>
2017-11-20 19:01:59 -0800

    Added variables project

---
+++ c#/Intro to c#/Variables/Variables/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            const float Pi = 3.14f;
+            Console.WriteLine(Pi);
+            byte number = 0;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "catty";
+            bool isWorking = false;
+            // type cw and press tab to use a code snippet for console.writeline
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+        }
+    }
+}
