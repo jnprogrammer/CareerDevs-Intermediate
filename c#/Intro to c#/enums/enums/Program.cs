3cd6c174691ddbbf3ec1be1109d829b2d60ad15d
jnprogrammer <joshuanparker@gmail.com>
2017-11-21 17:31:54 -0800

    Added refrence/value types

--- c#/Intro to c#/enums/enums/Program.cs
+++ c#/Intro to c#/enums/enums/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace enums
 {
+    public enum ShippingMethod
+    {
+        //always initilized values 
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodID = 2;
+            Console.WriteLine((ShippingMethod)methodID);
+
+            Console.WriteLine(method);
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod),methodName);
         }
     }
 }
