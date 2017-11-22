0e97986267a4c86b6c842ee66eb59f353cb76bde
jnprogrammer <joshuanparker@gmail.com>
2017-11-22 09:07:22 -0800

    Adding conditional exercises

--- c#/Intro to c#/Conditionals/Conditionals/Program.cs
+++ c#/Intro to c#/Conditionals/Conditionals/Program.cs
@@ -8,43 +8,57 @@ namespace Conditionals
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            bool isGoldCustomer = true;
+            var season = Season.Autumn;
 
-            ////////float price;
-            ////////if (isGoldCustomer)
-            ////////    price = 19.95f;
-            ////////else
-            ////////    price = 29.95f;
-
-            float price = (isGoldCustomer) ? 19.95f : 29.95f;
+            switch (season)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("Its autumn");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("Time to enjoy the Sun");
+                    break;
+                case Season.Winter:
+                    Console.WriteLine("ITs winter");
+                    break;
+                case Season.Spring:
+                    Console.WriteLine("ITs spring time");
+                    break;
+                default:
+                    Console.WriteLine("I don't understand the season");
+                    break;
+            }
         }
     }
+ //bool isGoldCustomer = true;
+
+            //float price;
+            //if (isGoldCustomer)
+            //    price = 19.95f;
+            //else
+            //    price = 29.95f;
+
+            // Ternary Operator
+            //float price = (isGoldCustomer) ? 19.95f : 29.95f;
+            //Console.WriteLine(price);
+      //int hour = 10;
+
+      //      if (hour > 0 && hour < 12)
+      //      {
+      //          Console.WriteLine("It's morning.");
+      //      }
+      //      else if (hour >= 12 && hour < 18)
+      //      {
+      //          Console.WriteLine("It's afternoon.");
+      //      }
+      //      else
+      //      {
+      //          Console.WriteLine("It's evening.");
+      //      }
+
 }
 
 
-/*
- * 
- * 
- * 
- * 
- *    int hour = 10;
-
-            if (hour > 0 && hour < 12)
-            {
-                Console.WriteLine("It's morning.");
-            }
-            else if (hour >= 12 && hour < 18)
-            {
-                Console.WriteLine("It's afternoon.");
-            }
-            else
-            {
-                Console.WriteLine("It's evening.");
-            }
- * 
- * 
- * 
- * 
- * /
