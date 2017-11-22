3974d1198ff2103ef092d6fcb1123f9a315b1615
jnprogrammer <joshuanparker@gmail.com>
2017-11-21 18:15:07 -0800

    working on conditionals

--- c#/Intro to c#/Conditionals/Conditionals/Program.cs
+++ c#/Intro to c#/Conditionals/Conditionals/Program.cs
@@ -10,6 +10,41 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            bool isGoldCustomer = true;
+
+            ////////float price;
+            ////////if (isGoldCustomer)
+            ////////    price = 19.95f;
+            ////////else
+            ////////    price = 29.95f;
+
+            float price = (isGoldCustomer) ? 19.95f : 29.95f;
         }
     }
 }
+
+
+/*
+ * 
+ * 
+ * 
+ * 
+ *    int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning.");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon.");
+            }
+            else
+            {
+                Console.WriteLine("It's evening.");
+            }
+ * 
+ * 
+ * 
+ * 
+ * /
