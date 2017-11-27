74f6d54eb929902ddc91f86595a77cc29389e8e0
jnprogrammer <joshuanparker@gmail.com>
2017-11-27 15:14:34 -0800

    finsihed Exercise 9

--- c#/Intro to c#/Exercies/Exercies/Program.cs
+++ c#/Intro to c#/Exercies/Exercies/Program.cs
@@ -10,38 +10,23 @@ namespace Exercies
     {
         static void Main(string[] args)
         {
-            var random = new Random();
-            int tries = 4, randomNumber = 0, convertedNum=0;
-            string userInput = "";
-            bool win = false;
-            Console.WriteLine("Pick a number between 1 and 10, guess right to win!");
-            Console.WriteLine(String.Format("You have {0} tries", tries));
-            randomNumber = 2; // random.Next(1, 10);
+            int[] ary = new int[100];
+            
+            Console.WriteLine("Enter a series of numbers seporated by a comma \",\"");
+            var input = Console.ReadLine();
+            var numbers = input.Split(',');
+            var max = Convert.ToInt32(numbers[0]);
 
-            do
+            foreach(var num in numbers)
             {
-                userInput = Console.ReadLine();
-                int.TryParse(userInput, out convertedNum);
-                if (convertedNum != randomNumber)
+                var number = Convert.ToInt32(num);
+                if (number > max)
                 {
-                    tries--;
-                    Console.WriteLine("Wrong guess Try again");
-                    Console.WriteLine(String.Format("You have {0} tries", tries));
-                    if (tries <= 0)
-                    {
-                        Console.WriteLine("You lost");
-                        break;
-                    }
+                    max = number;
                 }
-                else if (convertedNum == randomNumber)
-                {
-                    Console.WriteLine("You guess right, you've won!");
-                    win = true;
-                    break;
+            }
 
-                }
-            } while (tries <= 0 || win == false);
-
+            Console.WriteLine("The max is " + max);
         }
     }
 }
---
+++ c#/Intro to c#/Section5-Exercises/Exercise9.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ary = new int[100];
+
+            Console.WriteLine("Enter a series of numbers seporated by a comma \",\"");
+            var input = Console.ReadLine();
+            var numbers = input.Split(',');
+            var max = Convert.ToInt32(numbers[0]);
+
+            foreach (var num in numbers)
+            {
+                var number = Convert.ToInt32(num);
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+            Console.WriteLine("The max is " + max);
+        }
+    }
+}
