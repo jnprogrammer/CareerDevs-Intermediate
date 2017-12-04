eb5c71e209e244054b701723a669bed9ed7ab89b
jnprogrammer <joshuanparker@gmail.com>
2017-12-03 23:58:10 -0800

    Starting section 8 exercies

--- c#/Introduction_c#/Exercies/Exercies/Program.cs
+++ c#/Introduction_c#/Exercies/Exercies/Program.cs
@@ -1,59 +1,35 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/*
- * 
- * 
-  8.1- Write a program and ask the user to enter a few numbers separated by a hyphen. 
-  Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", 
-  display a message: "Consecutive"; otherwise, display "Not Consecutive".
- * 
- * 
- */
+
 namespace Exercies
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter in a few numbers seporated by hyphen - ");
-            var input = Console.ReadLine();
-            string numLine = "";
-            for(var i = input.Length; i > 0; i++)
+            var path = @"c:\somefile.jpg";
+            File.Copy(@"c:\temp\myFile.jpg", @"d:\temp\myfile.jpg",true);
+            File.Delete(path);
+
+            if(File.Exists(path))
             {
-                
+                //some code
             }
-            
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo(". . . ");
+            fileInfo.Delete();
+            if(fileInfo.Exists)
+            {
+                //Do more Code
+            }
            
-
-           
-            
-            // Creating timespan objects
-            var timeSpan = new TimeSpan(1,2,3);
-            var timeSpan2 = new TimeSpan(1,0,0);
-
-            var timeSpan3 = TimeSpan.FromHours(1);
-            var start = DateTime.Now;
-            var end = DateTime.Now.AddMinutes(2);
-            var duration = end - start;
-            Console.WriteLine("Duration: " + duration);
-
-            //Properties
-            Console.WriteLine("Minutes: " + timeSpan.Minutes);
-            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
-
-            //Add
-            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(7)));
-            Console.WriteLine("Add Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(23)));
-
-            //To string
-            Console.WriteLine("ToString" + timeSpan);
-
-            //Parse
-            Console.WriteLine("Parse: " + TimeSpan.Parse("01:12:23"));
         }
     }
 }
---
+++ c#/Introduction_c#/Section8-Exercises/Exercise8-1.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 
+ * 
+  8.1- Write a program and ask the user to enter a few numbers separated by a hyphen. 
+  Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", 
+  display a message: "Consecutive"; otherwise, display "Not Consecutive".
+ * 
+ * 
+ */
+namespace Exercies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter in a few numbers seporated by hyphen - ");
+            var input = Console.ReadLine();
+            string numLine = "";
+            for (var i = input.Length; i > 0; i++)
+            {
+
+            }
+
+
+
+
+
+            // Creating timespan objects
+            var timeSpan = new TimeSpan(1, 2, 3);
+            var timeSpan2 = new TimeSpan(1, 0, 0);
+
+            var timeSpan3 = TimeSpan.FromHours(1);
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
+
+            //Properties
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+
+            //Add
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(7)));
+            Console.WriteLine("Add Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(23)));
+
+            //To string
+            Console.WriteLine("ToString" + timeSpan);
+
+            //Parse
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:12:23"));
+        }
+    }
+}
