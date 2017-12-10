bc238732f9d209c4adbd5ca4fd6abfc1903132a2
jnprogrammer <joshuanparker@gmail.com>
2017-12-09 16:40:16 -0800

    completed Access modifiers

--- c#/Intermediate_c#/Classes/Class_accessModifiers.cs
+++ c#/Intermediate_c#/Classes/Class_accessModifiers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,7 +21,7 @@ namespace Intermediate_Exercies
         }
     }
 
-   class Program
+    class Program
     {
         static void Main(string[] args)
         {
