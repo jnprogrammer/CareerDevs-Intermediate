59a9f0583f421281b221508bb2a999a18b8dca78
jnprogrammer <joshuanparker@gmail.com>
2017-12-14 21:00:24 -0800

    fixing function to start and stop more than twice

--- c#/Intermediate_c#/Intermediate_Exercies/Intermediate_Exercies/Program.cs
+++ c#/Intermediate_c#/Intermediate_Exercies/Intermediate_Exercies/Program.cs
@@ -7,7 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 /*
- Exercise 1: Design a Stopwatch
+Exercise 1: Design a Stopwatch
+
 Design a class called Stopwatch. The job of this class is to simulate a stopwatch. It should
 provide two methods: Start and Stop. We call the start method first, and the stop method next.
 Then we ask the stopwatch about the duration between start and stop. Duration should be a
@@ -16,8 +17,7 @@ We should also be able to use a stopwatch multiple times. So we may start and st
 start and stop it again. Make sure the duration value each time is calculated properly.
 We should not be able to start a stopwatch twice in a row 
 (because that may overwrite the initial start time). 
-
- So the class should throw an InvalidOperationException if its started twice.
+So the class should throw an InvalidOperationException if its started twice.
 
  */
 namespace Intermediate_Exercies
@@ -25,10 +25,12 @@ namespace Intermediate_Exercies
     public class Stopwatch
     {
         public TimeSpan duration = new TimeSpan();
-        public TimeSpan second = new TimeSpan(0,0,1);
+        public TimeSpan startingDuration = new TimeSpan();
 
-    
-        public void Start()
+        public TimeSpan second = new TimeSpan(0,0,1);
+        public bool watchStopped = true;
+
+        public void StartStop()
         {
             do
             {
@@ -39,16 +41,19 @@ namespace Intermediate_Exercies
 
             } while (Console.ReadKey().Key != ConsoleKey.S);
             Console.WriteLine("STOPPED @: " + duration);
-
         }
 
-        public void Stop()
+        public void flipBool()
         {
-            
-        }
-        public void ShowDuration()
-        {
-
+            if (watchStopped==false)
+            {
+                watchStopped = true;
+            }
+            else if(watchStopped==true)
+            {
+                watchStopped = false;
+            }
+           
         }
     }
     class Program
@@ -58,11 +63,25 @@ namespace Intermediate_Exercies
             var stopWatch = new Stopwatch();
             bool watchStopped = true;
 
-            Console.WriteLine("Press s to start the stopWatch\n press s again");
-            if (Console.ReadKey().Key == ConsoleKey.S)
+            Console.WriteLine("Press s to start the stopWatch\npress s again to stop");
+
+            if (stopWatch.watchStopped)
             {
-                stopWatch.Start();
+                if (Console.ReadKey().Key == ConsoleKey.S)
+                {
+                    stopWatch.StartStop();
+                }
+                stopWatch.flipBool();
             } 
+            
+            if(!stopWatch.watchStopped)
+            {
+                if (Console.ReadKey().Key == ConsoleKey.S)
+                {
+                    stopWatch.StartStop();
+                }
+                stopWatch.flipBool();
+            }
         }
     }
 }
@@ -70,6 +89,7 @@ namespace Intermediate_Exercies
 
 
 /*
+ * Console.ReadKey().Key == ConsoleKey.S
  I like this function 
   public void Start()
     {      
