69efb5f889cba42d373972f18edf77728e0058ec
jnprogrammer <joshuanparker@gmail.com>
2017-12-16 08:42:31 -0800

    Fixed stopwatch exercise to start and stop using two functions

--- c#/Intermediate_c#/Intermediate_Exercies/Intermediate_Exercies/Program.cs
+++ c#/Intermediate_c#/Intermediate_Exercies/Intermediate_Exercies/Program.cs
@@ -13,6 +13,7 @@ Design a class called Stopwatch. The job of this class is to simulate a stopwatc
 provide two methods: Start and Stop. We call the start method first, and the stop method next.
 Then we ask the stopwatch about the duration between start and stop. Duration should be a
 value in TimeSpan. Display the duration on the console.
+
 We should also be able to use a stopwatch multiple times. So we may start and stop it and then
 start and stop it again. Make sure the duration value each time is calculated properly.
 We should not be able to start a stopwatch twice in a row 
@@ -24,36 +25,29 @@ namespace Intermediate_Exercies
 {
     public class Stopwatch
     {
-        public TimeSpan duration = new TimeSpan();
-        public TimeSpan startingDuration = new TimeSpan();
-
+        public TimeSpan clock = new TimeSpan();
         public TimeSpan second = new TimeSpan(0,0,1);
         public bool watchStopped = true;
 
-        public void StartStop()
+        public void Start()
         {
-            do
+            watchStopped = false;
+            while(watchStopped == false)
             {
-                while (!Console.KeyAvailable)
+                clock = clock.Add(second);
+                Console.WriteLine(clock);
+                while (Console.KeyAvailable)
                 {
-                    Console.WriteLine(duration = duration.Add(second));
+                    if (Console.ReadKey().Key == ConsoleKey.S)
+                    {
+                        watchStopped = true;
+                    }
                 }
-
-            } while (Console.ReadKey().Key != ConsoleKey.S);
-            Console.WriteLine("STOPPED @: " + duration);
+            }
         }
-
-        public void flipBool()
+        public void Stop()
         {
-            if (watchStopped==false)
-            {
-                watchStopped = true;
-            }
-            else if(watchStopped==true)
-            {
-                watchStopped = false;
-            }
-           
+            watchStopped = true;
         }
     }
     class Program
@@ -61,27 +55,22 @@ namespace Intermediate_Exercies
         static void Main(string[] args)
         {
             var stopWatch = new Stopwatch();
-            bool watchStopped = true;
 
-            Console.WriteLine("Press s to start the stopWatch\npress s again to stop");
-
-            if (stopWatch.watchStopped)
-            {
-                if (Console.ReadKey().Key == ConsoleKey.S)
-                {
-                    stopWatch.StartStop();
-                }
-                stopWatch.flipBool();
-            } 
             
-            if(!stopWatch.watchStopped)
+
+
+            while (!Console.KeyAvailable)
             {
-                if (Console.ReadKey().Key == ConsoleKey.S)
+               if(Console.ReadKey().Key == ConsoleKey.A)
                 {
-                    stopWatch.StartStop();
+                    stopWatch.Start();
                 }
-                stopWatch.flipBool();
+               
+
+
             }
+
+            
         }
     }
 }
